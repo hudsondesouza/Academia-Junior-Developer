02ae0d3040b4207f572f711ab96f84ff69413648
hudsonengineer <hudsonsouza@tutanota.com>
2026-04-25 06:38:49 -0300

    refactor: Converting submodules to normal folder

---
+++ CODE-I/III-Practice-task/JDCI-1.2-Operations/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ConsoleApp2.Services;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomNumber randomNumber = new RandomNumber();
+
+            double a = randomNumber.Lottery(0, 100);
+            double b = randomNumber.Lottery(0, 100);
+
+            if (a == 0 || b == 0)
+                throw new ArgumentException("Division by zero is not allowed.");
+
+            double result1 = a / b;
+            double result2 = b / a;
+
+            double max = Math.Max(result1, result2);
+
+            string highestResult;
+
+            if (result1 > result2)
+                result1 = highestResult;
+            
+
+            Console.ReadKey();
+        }
+    }
+}
